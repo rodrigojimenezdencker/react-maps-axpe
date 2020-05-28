5fd36b8b5a224d08acdfd7a8d818674ae6da1359
Rodrigo Fernando Jiménez Dencker <rodrigo.jimenez.developer@gmail.com>
2020-05-28 18:18:10 +0200

    Added markers history with redux.

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -2,20 +2,30 @@ import React, { Component } from 'react';
 import { withGoogleMap, GoogleMap, withScriptjs, Marker } from "react-google-maps";
 import Geocode from "react-geocode";
 import { Searcher } from '../Searcher/Searcher';
+import { connect } from "react-redux";
+import { addMarker } from '../../actions';
 
 const API_KEY = 'AIzaSyB4okjTTnndv_nqIU75IB54R2PZFfy1tVw';
 console.log(API_KEY)
 Geocode.setApiKey(API_KEY);
 
-class Map extends Component {
+const mapStateToProps = state => {
+	return {
+		markers: state.markers
+	}
+}
+
+const mapDispatchToProps = dispatch => {
+	return {
+		addMarker: (Marker) => dispatch(addMarker(Marker))
+	}
+}
+
+class connectedMap extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			mapPosition: {
-				lat: this.props.center.lat,
-				lng: this.props.center.lng
-			},
-			markerPosition: {
 				lat: this.props.center.lat,
 				lng: this.props.center.lng
 			}
@@ -29,15 +39,12 @@ class Map extends Component {
 		const
 			latValue = place.geometry.location.lat(),
 			lngValue = place.geometry.location.lng();
+		this.props.addMarker({ lat: latValue, lng: lngValue })
 		this.setState({
 			mapPosition: {
 				lat: latValue,
 				lng: lngValue
-			},
-			markerPosition: {
-				lat: latValue,
-				lng: lngValue
-			},
+			}
 		})
 	};
 
@@ -50,9 +57,12 @@ class Map extends Component {
 						defaultZoom={this.props.zoom}
 						defaultCenter={{ lat: this.state.mapPosition.lat, lng: this.state.mapPosition.lng }}
 					>
-						<Marker google={this.props.google}
-							position={{ lat: this.state.markerPosition.lat, lng: this.state.markerPosition.lng }}
-						/>
+						{this.props.markers.map(marker =>
+							<Marker google={this.props.google}
+								draggable={false}
+								position={{ lat: marker.lat, lng: marker.lng }}
+							/>
+						)}
 						<Searcher
 							onPlaceSelected={this.onPlaceSelected}
 							types={['(regions)']}
@@ -82,4 +92,5 @@ class Map extends Component {
 	}
 }
 
+const Map = connect(mapStateToProps, mapDispatchToProps)(connectedMap);
 export default Map
