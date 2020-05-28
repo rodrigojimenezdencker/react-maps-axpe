972e73d185ed7624441e0898150f4c416276ec94
Rodrigo Fernando Jiménez Dencker <rodrigo.jimenez.developer@gmail.com>
2020-05-28 17:52:54 +0200

    Created Map component with my API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Map from './Components/Map/Map';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        center={{ lat: 41.3922909, lng: 2.1655837 }}
+        height='100vh'
+        zoom={15}
+      />
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Map/Map.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { withGoogleMap, GoogleMap, withScriptjs, Marker } from "react-google-maps";
+import Autocomplete from 'react-google-autocomplete';
+import Geocode from "react-geocode";
+
+const API_KEY = 'AIzaSyB4okjTTnndv_nqIU75IB54R2PZFfy1tVw';
+console.log(API_KEY)
+Geocode.setApiKey(API_KEY);
+
+class Map extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			mapPosition: {
+				lat: this.props.center.lat,
+				lng: this.props.center.lng
+			},
+			markerPosition: {
+				lat: this.props.center.lat,
+				lng: this.props.center.lng
+			}
+		}
+	}
+
+	onPlaceSelected = (place) => {
+		if (!place.geometry) {
+			return false;
+		}
+		const
+			latValue = place.geometry.location.lat(),
+			lngValue = place.geometry.location.lng();
+		this.setState({
+			mapPosition: {
+				lat: latValue,
+				lng: lngValue
+			},
+			markerPosition: {
+				lat: latValue,
+				lng: lngValue
+			},
+		})
+	};
+
+
+	render() {
+		const CustomMap = withScriptjs(
+			withGoogleMap(
+				props => (
+					<GoogleMap google={this.props.google}
+						defaultZoom={this.props.zoom}
+						defaultCenter={{ lat: this.state.mapPosition.lat, lng: this.state.mapPosition.lng }}
+					>
+						<Marker google={this.props.google}
+							position={{ lat: this.state.markerPosition.lat, lng: this.state.markerPosition.lng }}
+						/>
+						<Autocomplete
+							className="searcher"
+							onPlaceSelected={this.onPlaceSelected}
+							types={['(regions)']}
+						/>
+					</GoogleMap>
+				)
+			)
+		);
+		let map;
+		if (this.props.center.lat !== undefined) {
+			map = <div>
+				<CustomMap
+					googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${API_KEY}&libraries=places`}
+					loadingElement={
+						<div style={{ height: `100%` }} />
+					}
+					containerElement={
+						<div style={{ height: this.props.height }} />
+					}
+					mapElement={
+						<div style={{ height: `100%` }} />
+					}
+				/>
+			</div>
+		}
+		return (map)
+	}
+}
+
+export default Map
